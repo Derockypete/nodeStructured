16ae6957eaa640fae4f5ccf5ec282f6acb69c8d1
Peter <35315724+Derockypete@users.noreply.github.com>
2021-02-23 15:55:58 +0100

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri:
+    process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    "mongodb://cbox-writable-test:2f9Vu8ksaMqWWg@ds251902.mlab.com:51902/hihimongotest",
+  stripe_connect_test_client_id: "YOUR_stripe_connect_test_client",
+  stripe_test_secret_key: "YOUR_stripe_test_secret_key",
+  stripe_test_api_key: "YOUR_stripe_test_api_key"
+};
+
+export default config;
